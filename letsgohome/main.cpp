3a56166f1963a561f22fdd7af8a601b3053c9910
Nusrat Nuriyev <nuriyevn@gmail.com>
2018-10-28 20:19:20 +0200

    hour intervals

--- letsgohome/main.cpp
+++ letsgohome/main.cpp
@@ -4,13 +4,34 @@ using namespace std;
 
 int main ()
 {
-    cout << "What time is it";
+    int start = 9, finish = 18;
+
+    cout << "What time is it?";
     int hour = 0;
     cin >> hour;
+
+    // && И
+    // 1 && 0 - 0
+    // 0 && 1 - 0
+    // 1 && 1 - 1
+    // 0 && 0 - 0
+    if (hour < finish  && hour >start )
+        cout << "Go working";
+    else if (hour > finish  || hour < start)
+    // || или
+    // 1 || 0 - 1   20
+    // 0 || 1 - 1
+    // 1 || 1 - 1
+    // 0 || 0 - 0
+
+
+
+
 
     if (hour > 20)
         cout << "Go home!";
     else if (hour <= 20)
-    cout << "Go to shop and buy bread";
+        cout << "Go to shop and buy bread";
+
 
 }
