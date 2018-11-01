a6a9c6c783b96dcfa7c7e11b58b17ccd5fe5795e
Anastasiia Nuriyeva <nastik9097@gmail.com>
2018-11-01 18:58:15 +0200

    Plus plus

--- test3.cpp
+++ test3.cpp
@@ -1,5 +1,59 @@
 //  Ділити число на два поки воно не стане менше одиниці.
 //  Число має бути більше 1.0
 //  Відповідь - кількіть скільи разів ми поділили на два щоб отримати резльтут менше ніж 1.0
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    float n = 0.0,i = 2000.0;
+    int count = 0;
+
+    while (n <= i)
+    {
+      cin >> n;
+      count = count +1;
+      cout << count << endl;
+
+
+    }
+
+
+}
+
+
+
+
+int main2()
+{
+    float n, i = 2000;
+    cin >> n;
+    int count =0;
+
+    while (i >= n)
+{
+        n = n * 2.0;
+        count = count +1;
+        cout << n << " - "<<count <<endl;
+}
+
+}
+
+int main1()
+{
+    float n, i = 1.0;
+    cin >> n;
+    int count = 0;
+
+    while (i <= n )
+    {
+        n = n / 2.0;
+        count = count + 1;
+        cout << n << " - "<< count << endl;
+    }
+}
+
+
 
 
